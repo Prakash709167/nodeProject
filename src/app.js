f37e33fef7613cfc2029fe70d2045e7b440f1fef
Prakash kumar <prakash709167@gmail.com>
2023-07-13 00:16:15 +0530

    this second day of work

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const path = require("path");
 require("./db/conn");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//setting the path
+const staticpath = path.join(__dirname, "../public")
+
+// middleware
+app.use('/css', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css")));
+app.use('/js', express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js")));
+app.use('/jq', express.static(path.join(__dirname, "../node_modules/jquery/dist")));
+app.use(express.static(staticpath))
+app.set("view engine","hbs");
+
+
 // routing
 // app.get(path , callback)
 app.get("/",(req,res)=> {
-    res.send("Hii I am awesome")
+    res.render("index");
+})
+
+app.get("/contact",(req,res)=> {
+    res.render("contact");
 })
 
 // server create
